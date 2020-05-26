541dfc3c66024abf85a782ae2539dddb44aa3659
Amr Morsy <amrmorsy2@gmail.com>
2020-05-26 23:12:31 +0200

    DB insert func

--- DBConnections/DB_Connection.go
+++ DBConnections/DB_Connection.go
@@ -1,26 +1,16 @@
-package MeetUp
+package DBConnections
 
 import (
 	"context"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
-	"log"
 	"time"
 )
 
 
-func CreateDBconnection(connectionString string) (*mongo.Client, error){
+func CreateDBconnection(connectionString string) (*mongo.Client, error, context.Context){
 	client, err := mongo.NewClient(options.Client().ApplyURI(connectionString))
-	if err != nil {
-		log.Fatal(err)
-		return client, err
-	}
 	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
 	err = client.Connect(ctx)
-	if err != nil {
-		log.Fatal(err)
-		return client, err
-	}
-	defer client.Disconnect(ctx)
-	return client, err
+	return client, err, ctx
 }
--- main.go
+++ main.go
@@ -1,1 +1,11 @@
-package MeetUp
+package main
+
+import (
+	"./DBConnections"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/insertintodb", DBConnections.DB_Insert_Student)
+	http.ListenAndServe(":8080", nil)
+}
