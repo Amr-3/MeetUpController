ada86219ed015df9b0524d2404ec77c35c5ef172
amrayman88 <amr_ayman88@hotmail.com>
2020-06-01 00:30:22 +0200

    integrated the place struct and changed to monolith

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"./DBConnections"
+	"./Place"
 	user "./User/model"
 	"github.com/gin-gonic/gin"
 	"net/http"
@@ -18,5 +19,16 @@ func main() {
 
 	}
 	router.Run(":9000")
+
+	p := Place.Place{
+		ID:          1,
+		Name:        "Primos",
+		Votes:       5,
+		Rating:      4.8,
+		Description: "the best pizza restaurant",
+		UserID:      []string{"1","2","3","4"},
+	}
+
+	DBConnections.DbInsert(p,"AyEsm")
 }
 
